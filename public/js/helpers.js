87a164b3969407b3012858ad5954057968baf5e9
Zak Fisher <zak.fisher@gmail.com>
2014-11-03 14:31:29 -0800

    deleted helpers.js

--- public/js/helpers.js
+++
@@ -1,5 +0,0 @@
-$.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
-    if (options.url.indexOf('http') == -1) {
-        options.url = 'http://api.westfield.io/api/' + options.url;
-    }
-});
